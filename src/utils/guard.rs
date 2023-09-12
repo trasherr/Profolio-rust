0184c0060c25e6f330e7457ff6362155b8e92132
Prakhar Singh <2000prakharsingh@gmail.com>
2023-09-12 18:59:30 +0530

    api response

--- src/utils/guard.rs
+++ src/utils/guard.rs
@@ -5,25 +5,23 @@ use axum::{
     middleware::Next,
     response::Response,
 };
+use crate::utils::api_error::APIError;
 use entity::user;
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
-pub async fn guard<T>(mut request: Request<T>, next: Next<T>) -> Result<Response, StatusCode> {
+pub async fn guard<T>(mut request: Request<T>, next: Next<T>) -> Result<Response, APIError> {
 
-    let token = request.headers().typed_get::<Authorization<Bearer>>().ok_or(StatusCode::UNAUTHORIZED)?.token().to_owned();
-    let claims = decode_jwt(token)?.claims;
-
-    let db = request.extensions().get::<DatabaseConnection>().ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    let token = request.headers().typed_get::<Authorization<Bearer>>().ok_or(APIError { error_code: None, message: "Authentication failed".to_owned(), status_code: StatusCode::BAD_REQUEST})?.token().to_owned();
+    let claims = decode_jwt(token).map_err(|err| APIError { error_code: None, message: err.to_string(), status_code: StatusCode::UNAUTHORIZED})?.claims;
+    let db = request.extensions().get::<DatabaseConnection>().ok_or(APIError { error_code: None, message: "Could not connect to database".to_owned(), status_code: StatusCode::INTERNAL_SERVER_ERROR})?;
 
     let user = user::Entity::find()
         .filter(user::Column::Email.eq(claims.email))
         .one(db)    
         .await
-        .map_err(|_error| {
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
+        .map_err(|err| APIError { error_code: None, message: err.to_string(), status_code: StatusCode::INTERNAL_SERVER_ERROR})?;
 
-    let Some(user) = user else {return Err(StatusCode::UNAUTHORIZED)};
+    let Some(user) = user else {return Err(APIError { error_code: None, message: "Invalid token".to_owned(), status_code: StatusCode::UNAUTHORIZED})};
 
     request.extensions_mut().insert(user);
 
